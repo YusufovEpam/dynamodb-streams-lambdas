b487d9dbe1c2805e339d65f43fffe398880481d4
YusufovEpam <82205487+YusufovEpam@users.noreply.github.com>
2021-06-02 19:07:28 +0300

    feat: SVC-339 Create dynamodb-streams-lambdas github repo

---
+++ deposits-validator/fn/src/index.spec.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+const sendDynamoRecordsToSqs = jest.fn();
+const mockDynamodbStreamsEventServiceEnqueuer = {
+  sendDynamoRecordsToSqs,
+};
+
+jest.mock('@endpoint/dynamodb-streams-event-service-enqueuer', () => mockDynamodbStreamsEventServiceEnqueuer);
+
+const { lambdaHandler } = require('./index');
+
+describe('lambda handler', () => {
+  let procEnv: NodeJS.ProcessEnv;
+
+  beforeEach(() => {
+    procEnv = process.env;
+    process.env = {
+      NODE_ENV: 'development',
+    };
+  });
+
+  afterEach(() => {
+    process.env = procEnv;
+  });
+
+  it('it should call sendDynamoRecordsToSqs', () => {
+    const event = {
+      Records: [],
+    };
+
+    lambdaHandler(event);
+    expect(sendDynamoRecordsToSqs).toHaveBeenCalledWith(
+      { tableName: 'Deposits', records: event.Records },
+      process.env.NODE_ENV,
+    );
+  });
+});
